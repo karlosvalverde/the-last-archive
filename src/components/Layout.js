70003ca3f58d25fad30586c1b89ba41acbcf16ba
Carlos Valverde <studio@carlos-valverde.com>
2022-09-11 21:49:04 +0200

    [ADD] React Router + Data fetch logic [IMP] Overall design

---
+++ src/components/Layout.js
@@ -0,0 +1,77 @@
+// import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+
+function Layout() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const randomColor = [
+    "primary", "secondary", "light", "warning", "success", "danger"
+  ]
+
+  const randomize = (items) => {
+    return items[Math.floor(Math.random()*items.length)];
+  };
+
+  const [modalShow, setModalShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          )
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container-fluid vh-100">
+      <div className='row row-cols-auto h-100 justify-content-between align-items-center
+      '>
+        {
+          loading &&
+          <div className='col-12'>
+              <h1 className='inter-xli text-center text-primary'><span className='p-5'>Loading ...</span></h1>
+          </div>
+        }
+        {
+          error &&
+          <div className='col-12'>
+              <h1 className='inter-xli text-center text-primary'><span className='p-5'>There is a problem fetching the data -  {error}</span></h1>
+          </div>
+        }
+        {
+          data &&
+            data.map(({ id, title }) => (
+              <div key={ id } className='col col-md-3'>
+                {/* <button className=''> */}
+                  <h2 className={`is-sticker bg-${randomize(randomColor)} inter-li border border-dark border-2 text-center text-dark p-5`}><span className='fw-bold'>{id}</span> - { title }</h2>
+                {/* </button> */}
+              </div>
+            ))
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import './style.scss';
+import './sass/style.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
